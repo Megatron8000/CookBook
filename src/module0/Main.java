4e85bd867c644de7aedbde15b2bb7604a480e249
Megatron8000 <barykin88@hotmail.com>
2022-01-05 11:08:40 +0400

    hello world

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -2,6 +2,7 @@ package module0;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        String s = "Hello";
+        System.out.println(s);
     }
 }
