f652f66223ce7228c422727b1b75a33658a76bc2
Megatron8000 <barykin88@hotmail.com>
2022-01-06 12:42:43 +0400

    if-else

--- src/module1/IfStatement.java
+++ src/module1/IfStatement.java
@@ -2,12 +2,17 @@ package module1;
 
 public class IfStatement {
     public void foo(){
-        int a, b;
+        int a, b, c, d;
         a = 5;
         b = 7;
 
         if (a < b){
             System.out.println("True");
         } else System.out.println("False");
+
+        c = a - b;
+        if (c <= 0){
+            System.out.println("С отрицательное или равно нулю");
+        } else System.out.println("С положительное или равно нулю");
     }
 }
