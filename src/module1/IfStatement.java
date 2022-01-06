429923593045adb6a06611b5cceda50e53dc8331
Megatron8000 <barykin88@hotmail.com>
2022-01-06 12:25:52 +0400

    if-else

---
+++ src/module1/IfStatement.java
@@ -0,0 +1,17 @@
+package module1;
+
+public class IfStatement {
+
+
+    public void foo(){
+        int a, b;
+        a = 5;
+        b = 7;
+
+        if (a < b){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
