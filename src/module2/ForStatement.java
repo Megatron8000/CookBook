b045bdd88b20818677a7a866df27edeb6525fff7
Megatron8000 <barykin88@hotmail.com>
2022-01-06 12:55:24 +0400

    for loop

---
+++ src/module2/ForStatement.java
@@ -0,0 +1,10 @@
+package module2;
+
+public class ForStatement {
+    public void foo(){
+        for (int i = 0; i < 50; i++){
+            System.out.println("Текущая переменная цикла " + i);
+            System.out.println("Цикл завершен");
+        }
+    }
+}
