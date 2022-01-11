b89001fce491006ee0a0e4db6136bca4f7cc742d
Megatron8000 <barykin88@hotmail.com>
2022-01-11 20:10:11 +0400

    floating dot

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -3,6 +3,7 @@ package module0;
 import module1.IfStatement;
 import module2.ForStatement;
 import module3.CharDemo;
+import module3.HypotDemo;
 
 import javax.swing.*;
 
@@ -10,8 +11,8 @@ public class Main {
    public static void main(String[] args) {
        /*JOptionPane.showMessageDialog(null, "Значок, что ты дурачок");*/
 
-       CharDemo charDemo = new CharDemo();
-       charDemo.foo();
+       HypotDemo hypotDemo = new HypotDemo();
+       hypotDemo.foo();
 
    } //method main
 }  //class Main
