1a22603e9fa7a36035fb007dbe64082f47ef664a
Megatron8000 <barykin88@hotmail.com>
2022-01-11 20:09:15 +0400

    floating dot

--- src/module3/HypotDemo.java
+++ src/module3/HypotDemo.java
@@ -1,4 +1,14 @@
 package module3;
 
 public class HypotDemo {
-}
+    public void foo(){
+        double cat1, cat2, hypotenuse;
+        cat1 = 3.6454;
+        cat2 = 5.9875454;
+
+        hypotenuse = Math.sqrt((cat1 * cat1) + (cat2 * cat2));
+
+        System.out.println(hypotenuse);
+    }
+
+}//class HypotDemo
