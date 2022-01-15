e57914871c874adc4d58db03cf70c1a4ab8d0bd9
Megatron8000 <barykin88@hotmail.com>
2022-01-15 10:17:15 +0400

    boolean demo

--- src/module3/BoolDemo.java
+++ src/module3/BoolDemo.java
@@ -5,5 +5,9 @@ public class BoolDemo {
         boolean b;
         b = false;
         System.out.println("Значение b " + b);
+        b = true;
+        System.out.println("Значение b " + b);
+
+        System.out.println("Значение выражения 10 < 9 " + (10 < 9));
     } //method
 } //class
