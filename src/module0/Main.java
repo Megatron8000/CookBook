903842ba755a89e2d5691629ee7e4ead50b37f97
Megatron8000 <barykin88@hotmail.com>
2022-01-15 10:13:32 +0400

    boolean demo

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -2,6 +2,7 @@ package module0;
 
 import module1.IfStatement;
 import module2.ForStatement;
+import module3.BoolDemo;
 import module3.CharDemo;
 import module3.HypotDemo;
 
@@ -11,9 +12,8 @@ public class Main {
    public static void main(String[] args) {
        /*JOptionPane.showMessageDialog(null, "Значок, что ты дурачок");*/
 
-       HypotDemo hypotDemo = new HypotDemo();
-       hypotDemo.foo();
-
+       BoolDemo boolDemo = new BoolDemo();
+       boolDemo.foo();
    } //method main
 }  //class Main
 
--- src/module3/BoolDemo.java
+++ src/module3/BoolDemo.java
@@ -1,4 +1,9 @@
 package module3;
 
 public class BoolDemo {
-}
+    public void foo(){
+        boolean b;
+        b = false;
+        System.out.println("Значение b " + b);
+    } //method
+} //class
