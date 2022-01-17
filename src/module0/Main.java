7e97408949dc94a502d4fc99722171c8783c8f5e
Megatron8000 <barykin88@hotmail.com>
2022-01-17 21:51:25 +0400

    dynamic variable initialization

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -2,10 +2,7 @@ package module0;
 
 import module1.IfStatement;
 import module2.ForStatement;
-import module3.BoolDemo;
-import module3.CharDemo;
-import module3.HypotDemo;
-import module3.StrDemo;
+import module3.*;
 
 import javax.swing.*;
 
@@ -13,8 +10,8 @@ public class Main {
    public static void main(String[] args) {
        /*JOptionPane.showMessageDialog(null, "Значок, что ты дурачок");*/
 
-       StrDemo strDemo = new StrDemo();
-       strDemo.foo();
+       Cylinder cylinder = new Cylinder();
+       cylinder.foo();
    } //method main
 }  //class Main
 
