0ab7e6bdefae5d6effb13f85363a1fd4439449a4
Megatron8000 <barykin88@hotmail.com>
2022-01-17 21:50:25 +0400

    dynamic variable initialization

--- src/module3/Cylinder.java
+++ src/module3/Cylinder.java
@@ -2,6 +2,10 @@ package module3;
 //демонстрация динамической инициализации переменной. в качестве значения используется произведение других переменных
 public class Cylinder {
     public void foo(){
+        double radius = 5;
+        double height = 21.66;
 
+        double volume = Math.PI * radius * radius * height;
+        System.out.println(volume);
     } //method
 } //class
