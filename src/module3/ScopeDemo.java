b602861ac025b815eb5979886232669e1bc42f1f
Megatron8000 <barykin88@hotmail.com>
2022-01-17 22:10:20 +0400

    variable scope demonstration

--- src/module3/ScopeDemo.java
+++ src/module3/ScopeDemo.java
@@ -2,7 +2,10 @@ package module3;
 // области видимости переменных
 public class ScopeDemo {
     public void foo(){
-        int i = 1;
+        int i = 1; //эта переменная будет доступна в любом месте метода foo, после инициализации
         System.out.println("До вложенного блока I = " + i);
+        {
+            int j = 3; //эта переменная доступна только внутри фигурных скобок
+        }
     } //method
 } //class
