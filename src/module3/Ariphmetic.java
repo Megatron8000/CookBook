056dbc025d35dc0c3258e4de4ac34b2d8cf2c233
Megatron8000 <barykin88@hotmail.com>
2022-01-22 10:01:43 +0400

    1

--- src/module3/Ariphmetic.java
+++ src/module3/Ariphmetic.java
@@ -2,6 +2,12 @@ package module3;
 
 public class Ariphmetic {
     public void foo(){
+        int iresult, iremain;
+        double dresult, dremain;
 
+        iresult = 10 / 3; //деление целых чисел
+        iremain = 10 % 3; //остаток от деления
+        System.out.println(iresult);
+        System.out.println(iremain);
     }
 }
