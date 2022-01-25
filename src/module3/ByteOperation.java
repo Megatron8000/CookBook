de34424ac05d26c687e992b297a57ca1c2973df3
Megatron8000 <barykin88@hotmail.com>
2022-01-25 07:22:20 +0400

    2

---
+++ src/module3/ByteOperation.java
@@ -0,0 +1,21 @@
+package module3;
+
+public class ByteOperation {
+    public void foo(){
+        int x = 0;
+        int y = 1;
+
+        int a = x & y;
+        System.out.println("Результат операции 0 & (AND) 1 будет " + a);
+
+        int b = x | y;
+        System.out.println("Результат операции 0 | (OR) 1 будет " + b);
+
+        int c = x ^ y;
+        System.out.println("Результат операции 0 ^ (XOR) 1 будет " + c);
+
+        int d = ~y;
+        System.out.println("Результат операции ~(NOT) y будет " + c);
+
+    }
+}
