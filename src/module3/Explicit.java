c01c6455ad6aa22017d4e5518c4b6a1cde95989e
Megatron8000 <barykin88@hotmail.com>
2022-01-27 06:41:32 +0400

    1

--- src/module3/Explicit.java
+++ src/module3/Explicit.java
@@ -4,5 +4,7 @@ public class Explicit {
     public void foo(){
         int i = 10;
         double d = i; // приведение переменной одного типа к другому
+        System.out.println(i);
+        System.out.println(d);
     }
 }
