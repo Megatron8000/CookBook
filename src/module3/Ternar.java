6deedea7702d8e288a1a65ae93050c79b454df5e
Megatron8000 <barykin88@hotmail.com>
2022-01-27 06:09:39 +0400

    1

--- src/module3/Ternar.java
+++ src/module3/Ternar.java
@@ -4,6 +4,7 @@ public class Ternar {
     public void foo(){
         for (int n = -5; n <= 5; n++){
             int a = n < 0 ? -n : n;
+            System.out.println(a);
         }
     }//method
 }//class
