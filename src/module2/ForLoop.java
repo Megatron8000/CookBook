308052d6ba81b8fc32e6545b8b8ec98166fbf1dd
Megatron8000 <barykin88@hotmail.com>
2022-01-30 12:19:46 +0400

    for loop

--- src/module2/ForLoop.java
+++ src/module2/ForLoop.java
@@ -2,8 +2,12 @@ package module2;
 
 public class ForLoop {
     public void foo() {
-        for (int x = 100; x > 0; x -= 5){
+      /*  for (int x = 100; x > 0; x -= 5){
             System.out.println(x);
-        }
+        }// first for loop*/
+        for (int i = 10, j = 1; i > 0; i--, j++){
+            System.out.println("Текущее значение i " + i);
+            System.out.println("Текущее значение j " + j);
+        }// second for loop
     }
 }
