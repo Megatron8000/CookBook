a33a9dbfb9d3e343763a512f1876a37a65787f35
Megatron8000 <barykin88@hotmail.com>
2022-02-01 06:52:46 +0400

    while loop

--- src/module2/WhileLoop.java
+++ src/module2/WhileLoop.java
@@ -2,6 +2,10 @@ package module2;
 
 public class WhileLoop {
     public void foo(){
-
+        char ch = 'а';
+        while (ch <= 'я'){ //цикл будет выполняться, пока условие истина
+            ch++;
+            System.out.println(ch);
+        }
     }
 }
