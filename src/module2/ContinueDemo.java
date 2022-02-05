dd7084755117ff17412f2af9cae99d4c7991df62
Megatron8000 <barykin88@hotmail.com>
2022-02-05 10:07:22 +0400

    break demo

--- src/module2/ContinueDemo.java
+++ src/module2/ContinueDemo.java
@@ -3,7 +3,7 @@ package module2;
 public class ContinueDemo {
     public void foo(){
         for (int n = 0; n < 10; n++){
-            if ((n % 2) == 0) continue;
+            if ((n % 2) == 0) continue; //по этому условию итерации цикла, в которых остаток от деления n на 2 == 0 будет пропущены
             System.out.println(n);
         }
     }
