8f897fa0a6a54cc2b71ee3eec9fe87918a17637a
Megatron8000 <barykin88@hotmail.com>
2022-02-05 09:55:25 +0400

    break demo

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -1,10 +1,7 @@
 package module0;
 
 import module1.IfStatement;
-import module2.DoWhileDemo;
-import module2.ForLoop;
-import module2.ForStatement;
-import module2.WhileLoop;
+import module2.*;
 import module3.*;
 
 import javax.swing.*;
@@ -13,7 +10,8 @@ public class Main {
    public static void main(String[] args) {
        /*JOptionPane.showMessageDialog(null, "Значок, что ты дурачок");*/
 
-
+       BreakDemo breakDemo = new BreakDemo();
+       breakDemo.foo();
 
    } //method main
 }  //class Main
