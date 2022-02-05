37785da69c31c295df8926a901e03e1bf5e2fe02
Megatron8000 <barykin88@hotmail.com>
2022-02-05 09:52:49 +0400

    command break demo

--- src/module2/BreakDemo.java
+++ src/module2/BreakDemo.java
@@ -2,6 +2,10 @@ package module2;
 
 public class BreakDemo {
     public void foo(){
-
-    }
-}
+        int m = 25;
+        for (int i = 0; i <= m; i++){
+            if (i * i >= m) break;
+            System.out.println("i равно " + i + ", i в квадрате равно " + i * i);
+        }//for
+    }//method foo
+}//class BreakDemo
