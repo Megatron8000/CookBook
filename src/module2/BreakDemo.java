f3b3a849b1551ceef13b62bd7323a4c278b952eb
Megatron8000 <barykin88@hotmail.com>
2022-02-05 09:59:48 +0400

    break demo

--- src/module2/BreakDemo.java
+++ src/module2/BreakDemo.java
@@ -4,7 +4,7 @@ public class BreakDemo {
     public void foo(){
         int m = 25;
         for (int i = 0; i <= m; i++){
-            if (i * i >= m) break;
+            if (i * i >= m) break; // при выполнении цикла проверяется данное условие, если варыжение true цикл прерывается
             System.out.println("i равно " + i + ", i в квадрате равно " + i * i);
         }//for
     }//method foo
