fe831110b0b37ec0ba79617d90f559a61b794538
Megatron8000 <barykin88@hotmail.com>
2022-02-07 20:44:42 +0400

    java class demo

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -3,6 +3,7 @@ package module0;
 import module1.IfStatement;
 import module2.*;
 import module3.*;
+import module4.Vehicle;
 
 import javax.swing.*;
 
@@ -10,8 +11,11 @@ public class Main {
    public static void main(String[] args) {
        /*JOptionPane.showMessageDialog(null, "Значок, что ты дурачок");*/
 
-       ContinueDemo continueDemo = new ContinueDemo();
-       continueDemo.foo();
+       Vehicle vehicle = new Vehicle();
+       vehicle.name = "CAR";
+       vehicle.setMaxspeed(220);
+       System.out.println(vehicle.name);
+       System.out.println(vehicle.getMaxspeed());
 
    } //method main
 }  //class Main
