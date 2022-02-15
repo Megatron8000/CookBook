b1fe296fd641d784466c0a7ca4e476099fca1c40
Megatron8000 <barykin88@hotmail.com>
2022-02-15 06:21:46 +0400

    return

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -3,6 +3,7 @@ package module0;
 import module1.IfStatement;
 import module2.*;
 import module3.*;
+import module4.ReturnDemo;
 import module4.Vehicle;
 
 import javax.swing.*;
@@ -11,16 +12,8 @@ public class Main {
    public static void main(String[] args) {
        /*JOptionPane.showMessageDialog(null, "Значок, что ты дурачок");*/
 
-       Vehicle cabriolet = new Vehicle();
-       cabriolet.setMaxspeed(220);
-
-       Vehicle bus = new Vehicle();
-       bus.setMaxspeed(80);
-
-       double time = 0.76;
-
-       cabriolet.distance(time);
-       bus.distance(time);
+       ReturnDemo rd = new ReturnDemo();
+       rd.foo();
 
    } //method main
 }  //class Main
