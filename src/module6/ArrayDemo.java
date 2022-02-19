6cfae9a9011ffbec697a1599ebbcbc4a44c0d674
Megatron8000 <barykin88@hotmail.com>
2022-02-19 12:15:38 +0400

    array

--- src/module6/ArrayDemo.java
+++ src/module6/ArrayDemo.java
@@ -3,5 +3,15 @@ package module6;
 public class ArrayDemo {
     public void foo(){
         int count[] = new int[10]; //одноменрый массив
+        int table[][] = new int[5][4]; //многомерный массив
+        int guests[][] = new int[7][]; //произвольный массив
+        guests[0] = new int[89];
+        guests[1] = new int[32];
+        guests[2] = new int[39];
+        guests[3] = new int[87];
+        guests[4] = new int[99];
+        guests[5] = new int[14];
+        guests[6] = new int[16];
+        System.out.println(guests);
     }
 }
