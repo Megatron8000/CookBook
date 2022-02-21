cb24b1a2b636b04a5621ce26e475eae0798a17a8
Megatron8000 <barykin88@hotmail.com>
2022-02-21 06:40:28 +0400

    add condition for WHEELS

--- src/module4/Vehicle.java
+++ src/module4/Vehicle.java
@@ -3,11 +3,13 @@ package module4;
 public class Vehicle {
     private int maxspeed;
     private String color;
+    private  int wheels;
 
 
     public Vehicle(int maxspeed, String color){
         this.maxspeed = maxspeed; //ключевое слово this используется когда  имя локальной переменной совпадает с иманем переменной члена класса
         this.color = color;
+        this.wheels = wheels;
     }
 
     public double distance(double time){
@@ -29,6 +31,17 @@ public class Vehicle {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        if ((wheels < 2 ) || (wheels > 18)) {
+            System.out.println("Invalid wheels value");
+        }
+        this.wheels = wheels;
     }
 }
 
