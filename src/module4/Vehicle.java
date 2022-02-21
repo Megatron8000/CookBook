f23fff6da0ca6fbdc13bf98ac49b239a61990e13
Megatron8000 <barykin88@hotmail.com>
2022-02-21 06:28:40 +0400

    add getters and setters

--- src/module4/Vehicle.java
+++ src/module4/Vehicle.java
@@ -2,7 +2,7 @@ package module4;
 
 public class Vehicle {
     private int maxspeed;
-    public String color;
+    private String color;
 
 
     public Vehicle(int maxspeed, String color){
@@ -13,6 +13,22 @@ public class Vehicle {
     public double distance(double time){
         double value = this.maxspeed * time;
         return value;
+    }
+
+    public int getMaxspeed() {
+        return maxspeed;
+    }
+
+    public void setMaxspeed(int maxspeed) {
+        this.maxspeed = maxspeed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
 
