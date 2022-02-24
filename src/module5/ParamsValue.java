6f86e3170693ea17919590b05e73868221320ebf
Megatron8000 <barykin88@hotmail.com>
2022-02-24 21:19:54 +0400

    11

--- src/module5/ParamsValue.java
+++ src/module5/ParamsValue.java
@@ -1,8 +1,11 @@
 package module5;
 
 public class ParamsValue {
-    public void call(int x, int y){
-        x = x + y;
+    public void call(int x, int y){ //передача параметров по значению
+        x = x + 55;
         y = y + 1;
+
+        System.out.println(x);
+        System.out.println(y);
     }
 }
