91394eb793a9b4bee30d1dfbf4284100a651e20f
Megatron8000 <barykin88@hotmail.com>
2022-03-07 11:16:51 +0400

    4545

---
+++ src/module2/ForFactorial.java
@@ -0,0 +1,12 @@
+package module2;
+
+public class ForFactorial {
+    //вычисление факториала в цикле
+    static long computeI(int n) {
+        long result = 1;
+        for (int i = 0; i <= n; i++)
+            result *= i;
+        return result;
+
+    }
+}
--- src/module4/Factorial.java
+++ src/module4/Factorial.java
@@ -2,4 +2,13 @@ package module4;
 
 public class Factorial {
 
-}
+    //вычисление факториала рекурсивным способом
+        static long computeR(int o){
+            if (o == 1) return  o;
+            return computeR(o - 1) * o;
+           }
+
+       }
+
+
+
