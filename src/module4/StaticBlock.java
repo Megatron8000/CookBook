87750a53779ed485a4725f7dda0c231119b0e278
Megatron8000 <barykin88@hotmail.com>
2022-03-09 19:10:47 +0400

    21

--- src/module4/StaticBlock.java
+++ src/module4/StaticBlock.java
@@ -2,16 +2,14 @@ package module4;
 
 public class StaticBlock {
     public static double rootOf9; //корень из 9
-    public static double rootOf27; //корень из 27
-    int num;
+    public int num;
 
-    static {
+   static {
         System.out.println("Инициализация статических членов класса...");
         rootOf9 = Math.sqrt(9.0);
-        rootOf27 = Math.sqrt(27.0);
     }
 
-    StaticBlock(int num) {
+   public StaticBlock(int num) {
         System.out.println("Инициализация переменных объекта класса StaticBlock...");
         this.num = num;
     }
