0208edb9ed2f9f4e8cb23b281796f656b0cef104
Megatron8000 <barykin88@hotmail.com>
2022-03-12 10:15:19 +0400

    88

--- src/module8/Auto.java
+++ src/module8/Auto.java
@@ -5,7 +5,9 @@ import module4.Vehicle;
 public class Auto extends Vehicle {
     private boolean sunroof;
 
-
+       public Auto(boolean sunroof){
+            super(330, "green", 4); // вызов конструктора класса Vehicle
+        }
 
     public boolean isSunroof() {
         return sunroof;
