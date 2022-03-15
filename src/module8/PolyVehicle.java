67003d908ccf08f8c1eaf43b8445dc1aad5949b6
Megatron8000 <barykin88@hotmail.com>
2022-03-15 20:28:43 +0400

    11

--- src/module8/PolyVehicle.java
+++ src/module8/PolyVehicle.java
@@ -6,10 +6,11 @@ public class PolyVehicle {
     Auto auto = new Auto(true); // экземпляр подкласса
     Vehicle vehicle = new Vehicle(); // экземпляр класса
 
+
     public void foo(){
-         Vehicle[] p = {auto, vehicle}; //помещаем объекты в массив, p[0] = auto, p[1] = vehicle;
-        for (int i = 0; i < p.length; i++){
-            System.out.println(p[i].toString());
-        } //динамический выбор версии переопределенного метода toString
+         Vehicle[] p = {vehicle, auto}; //помещаем объекты в массив, p[0] = auto, p[1] = vehicle;
+        System.out.println(p[0].toString());  // вызовет toString из vehicle
+        System.out.println(p[1].toString());  // вызовет toString из auto
+        } // выбор версии переопределенного метода toString
     }
-}
+
