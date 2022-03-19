438c4ce4cb6cfb93cb0ef711f1c4f2aab58b0254
Megatron8000 <barykin88@hotmail.com>
2022-03-19 10:43:17 +0400

    4

---
+++ src/module9/Triangle.java
@@ -0,0 +1,19 @@
+package module9;
+
+public class Triangle extends Shape {
+    int cat1; // первый катет
+    int cat2; // второй катет
+
+    Triangle (int cat1, int cat2){
+        this.cat1 = cat1;
+        this.cat2 = cat2;
+    } //конструктор Triangle
+
+    public String toString(){
+        return "Трейгольник";
+    }
+
+    double area(){
+        return ((cat1 * cat2) / 2.0);
+    } // реализация метода area() для класса Triangle
+}
