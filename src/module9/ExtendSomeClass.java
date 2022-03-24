6f03f0499eac653116a0fb830f4749ad524e4990
Megatron8000 <barykin88@hotmail.com>
2022-03-24 18:42:30 +0400

    55

---
+++ src/module9/ExtendSomeClass.java
@@ -0,0 +1,7 @@
+package module9;
+
+public class ExtendSomeClass extends SomeClass {
+    void method(){ // ошибка, т.к. метод в SomeClass окончательный
+        System.out.println("Хуй тебе");
+    }
+}
