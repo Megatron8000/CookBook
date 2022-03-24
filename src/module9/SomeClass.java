e10e686614f8959e93648db1cb0817a7172be90d
Megatron8000 <barykin88@hotmail.com>
2022-03-24 18:45:11 +0400

    55

--- src/module9/SomeClass.java
+++ src/module9/SomeClass.java
@@ -1,7 +1,7 @@
 package module9;
 
 public class SomeClass {
-    final void method(){  //метод final не может быть переопределен в другом классе
+    public final void method(){  //метод final не может быть переопределен в другом классе
         System.out.println("Окончательный метод");
     }
 }
