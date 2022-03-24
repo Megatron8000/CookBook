9f5fb9b0df988ba435c7c26d0bb87e8d4aab325e
Megatron8000 <barykin88@hotmail.com>
2022-03-24 18:31:07 +0400

    55

---
+++ src/module9/ConstMath.java
@@ -0,0 +1,7 @@
+package module9;
+
+final class ConstMath { //от класса с модификатором final нельзя наследовать
+    public static final double PI = 3.14;
+    public static final double E = 2.718;
+
+}
