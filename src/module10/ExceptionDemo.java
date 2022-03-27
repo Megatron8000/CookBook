637cda9f22c9cf19635a6650fda58f9792d9091b
Megatron8000 <barykin88@hotmail.com>
2022-03-27 10:57:53 +0400

    32

--- src/module10/ExceptionDemo.java
+++ src/module10/ExceptionDemo.java
@@ -1,10 +1,11 @@
 package module10;
 
 public class ExceptionDemo {
-    private static double divide(double dividend, double divisor)
+    public double divide(double dividend, double divisor)
         throws DivisionByZeroException{
         if(divisor == 0)
             throw new DivisionByZeroException(); //если делитель равен 0, выполнится создание объекта класса DivisionByZeroException
         return dividend/divisor;
     }
-}
+    }
+
