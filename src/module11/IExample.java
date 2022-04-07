063599693c4351772948e39a8aa27e251bb75e82
Megatron8000 <barykin88@hotmail.com>
2022-04-07 15:15:27 +0400

    56565

--- src/module11/IExample.java
+++ src/module11/IExample.java
@@ -3,11 +3,11 @@ package module11;
 public class IExample implements B {
     @Override
     public void methodB() {
-
+        System.out.println("Метод А");
     }
 
     @Override
     public void methodA() {
-
+        System.out.println("Метод Б");
     }
 }
