4f045019484fb43088ca299bef7c8813fb1482b9
Megatron8000 <barykin88@hotmail.com>
2022-04-11 06:36:01 +0400

    323232

--- src/module12/Str.java
+++ src/module12/Str.java
@@ -1,6 +1,11 @@
 package module12;
 
 public class Str {
-
-    StringBuffer sb = new StringBuffer(55);
+        public String foo(){
+            StringBuffer sb = new StringBuffer(55); //класс позволяющий оптимизировать работу с памятью
+            sb.append("Первая строка символов ");
+            sb.append("Еще одна строка");
+            String s3 = sb.toString();
+            return s3;
+        }
 }
