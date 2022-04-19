2465d1ccc0336e62fe0df8f273c77ce5880c8384
Megatron8000 <barykin88@hotmail.com>
2022-04-19 20:03:09 +0400

    6969

---
+++ src/module12/IntString.java
@@ -0,0 +1,10 @@
+package module12;
+
+public class IntString {
+    public void foo(){
+        //преобразование целого числа в строку
+            int valueInt = 1001;
+            String valueString = Integer.toString(valueInt);
+            System.out.println(valueString);
+    }
+}
