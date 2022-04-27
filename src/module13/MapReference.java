447cf9e55320eb7b6cf7c7a1a0d418d852d38d68
Megatron8000 <barykin88@hotmail.com>
2022-04-27 19:36:20 +0400

    1212121

---
+++ src/module13/MapReference.java
@@ -0,0 +1,25 @@
+package module13;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapReference {
+    public void foo(){
+        //создание словаря
+        Map map = new HashMap();
+
+        //создание класса оболочки для типа double
+        Double refD = new Double(1.23);
+
+        //добавление объекта в коллекцию
+        map.put("key", refD);
+
+        //получение класса-оболочки из коллекции
+        refD = (Double)map.get("key");
+
+        //получение значения double из объекта оболочки коллекции
+        double dd = refD.doubleValue();
+
+        System.out.println(dd);
+    }
+}
