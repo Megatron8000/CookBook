44ebcb5bd28c63af9c60dae7b44ea40133d89bde
Megatron8000 <barykin88@hotmail.com>
2022-04-28 19:25:58 +0400

    777777

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -200,16 +200,7 @@ public class Main {
       MapReference mr = new MapReference();
       mr.foo();
 
-      List spisok = new LinkedList(); //создание связанного списка
-      spisok = new ArrayList(); //создание списка массива
 
-      spisok.add("ss"); //добавление элемента в список
-      spisok.add(0, "dd"); //добавление элемента в конкретное место в списке
-
-      Object element = spisok.get(spisok.size() - 1); //получение последнего элемента в списке
-      System.out.println(element);
-      element = spisok.get(0); //получение элемента по указанному индексу
-      System.out.println(element);
 
   } //method main
 }  //class Main
