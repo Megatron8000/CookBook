27241ac7eed519e247d18b79389cb048c3bfbd49
Megatron8000 <barykin88@hotmail.com>
2022-05-01 17:20:17 +0400

    6565656565

--- src/module13/Sort.java
+++ src/module13/Sort.java
@@ -7,11 +7,15 @@ import java.util.List;
 public class Sort {
     public void foo() {
         //создание списка из строк
-        String[] srtArr = new String[] {"x", "F", "f"};
+        String[] srtArr = new String[] {"x", "F", "a"};
         List lst = Arrays.asList(srtArr);
 
         //простая сортировка
         Collections.sort(lst);
+        System.out.println(lst);
+
+        //сортрировка в обратном порядке
+        Collections.sort(lst, Collections.reverseOrder());
         System.out.println(lst);
 
 
