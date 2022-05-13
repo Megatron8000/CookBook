65445094e9489fc8c65abb03e268ba0e92cf34ad
Megatron8000 <barykin88@hotmail.com>
2022-05-13 19:04:31 +0400

    32323232

--- src/module14/EndlessWhile.java
+++ src/module14/EndlessWhile.java
@@ -2,6 +2,9 @@ package module14;
 
 public class EndlessWhile {
     public void foo(){
-
+        boolean f = 2 < 5; //в ходе выполения программы значение f может измениться, поэтому цикл while  не будет бесконечным
+        while (f){
+            System.out.println("AXAXAXAXA");
     }
+  }
 }
