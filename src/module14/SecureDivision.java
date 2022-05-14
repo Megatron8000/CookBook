a92dc1354d0a18135ee89818ae3156a50d55bf68
Megatron8000 <barykin88@hotmail.com>
2022-05-14 08:07:24 +0400

    69696969

--- src/module14/SecureDivision.java
+++ src/module14/SecureDivision.java
@@ -2,7 +2,7 @@ package module14;
 
 public class SecureDivision {
     public void foo(){
-        for (int divisor = -15; divisor <= 15; divisor++){
+        for (int divisor = -5; divisor <= 5; divisor++){
             //использование тернарного оператора для защиты от деления на ноль
             int result = divisor != 0 ? 100 / divisor : 0;
             if (result != 0){
