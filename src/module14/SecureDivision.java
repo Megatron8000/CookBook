af01def0f1eba4510193b81444060d332f9403f2
Megatron8000 <barykin88@hotmail.com>
2022-05-14 08:01:37 +0400

    69696969

--- src/module14/SecureDivision.java
+++ src/module14/SecureDivision.java
@@ -2,6 +2,9 @@ package module14;
 
 public class SecureDivision {
     public void foo(){
+        for (int divisor = -15; divisor <= 15; divisor++){
+            //использование тернарного оператора для защиты от деления на ноль
 
+        }
     }
 }
