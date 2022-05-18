f837c0bf70a889440ec3fac6daaead8c9a3bcaf1
Megatron8000 <barykin88@hotmail.com>
2022-05-18 06:31:45 +0400

    989898989

--- src/module14/UpCase.java
+++ src/module14/UpCase.java
@@ -2,6 +2,16 @@ package module14;
 
 public class UpCase {
     public void foo(){
+        //перевод строчных букв к верхнему регистру
+        char chh;
 
+        for (int i = 0; i < 10; i++){
+            chh = (char) ('a' + i);
+            System.out.println(chh + ";");
+
+            //команда отключающая шестой бит
+            chh = (char) ((int) chh & 0xffdf);
+            System.out.println(chh + " ");
+        }
     }
 }
