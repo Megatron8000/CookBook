80d7888408f2d60b90725349de21a3b2a9436242
Megatron8000 <barykin88@hotmail.com>
2022-05-19 19:44:04 +0400

    989898

--- src/module14/Base64Demo.java
+++ src/module14/Base64Demo.java
@@ -1,7 +1,14 @@
 package module14;
 
 public class Base64Demo {
-    public void foo(){
+    public void foo(String[] args){
+//программа должна быть запущена как минимум с одним параметром - строкой подлежащей шифрованию
 
+        if(args.length == 0){
+            System.out.println("Usage: Base64 text");
+            System.exit(1);
+
+
+        }
     }
 }
