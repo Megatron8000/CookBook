0ded2cbc83f47ce802d4dfac59fc09b0afa5ad05
Megatron8000 <barykin88@hotmail.com>
2022-05-19 19:55:38 +0400

    989898

--- src/module14/Base64Demo.java
+++ src/module14/Base64Demo.java
@@ -7,9 +7,10 @@ import java.util.Base64;
 
 public class Base64Demo {
     public void foo(){
-
+//кодирование строки
         byte[] encodedBytes = Base64.getEncoder().encode("Test".getBytes());
         System.out.println("encodedBytes " + new String(encodedBytes));
+//декодирование строки
         byte[] decodedBytes = Base64.getDecoder().decode(encodedBytes);
         System.out.println("decodedBytes " + new String(decodedBytes));
     }
