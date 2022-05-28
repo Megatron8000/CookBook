653fc9c24f3fd7c24cede94ca20de8b00dcbec7c
Megatron8000 <barykin88@hotmail.com>
2022-05-28 09:38:54 +0400

    65645456

--- src/module14/FactorialDemo.java
+++ src/module14/FactorialDemo.java
@@ -7,11 +7,11 @@ public class FactorialDemo {
 
         System.out.println("Факториал рассчитанный в цикле:");
         for (int i = 6; i > 0; i--){
-            System.out.println(i + "! " + f.compoteI(i));
+            System.out.println(i + "! " + f.computeI(i));
         }
         System.out.println("Факториал рассчитаный при помощий рекурсии:");
         for (int j = 1; j <= 6; j++){
-            System.out.println(j + "! " + f.computeR(j));
+            System.out.println(j + "! " + f.computeJ(j));
         }
     }
 }
