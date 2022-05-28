b91ed456bf4cd38eb4fe09b311dbececd3f825d2
Megatron8000 <barykin88@hotmail.com>
2022-05-28 09:20:19 +0400

    65645456

--- src/module14/Factorial.java
+++ src/module14/Factorial.java
@@ -2,4 +2,15 @@ package module14;
 
 public class Factorial {
 
+    //метод вычисляющий факториал при помощи цикла
+    long compoteI(int n){
+        long result = 1;
+
+        for (int i = 1; i <= n; i++)
+            result *= i;
+        return result;
+    }
+
+    //метод
+
 }
