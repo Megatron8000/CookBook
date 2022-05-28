444eba4cf05bf3cbd9b19bb52f3459df12317132
Megatron8000 <barykin88@hotmail.com>
2022-05-28 09:33:41 +0400

    65645456

--- src/module14/FactorialDemo.java
+++ src/module14/FactorialDemo.java
@@ -9,6 +9,9 @@ public class FactorialDemo {
         for (int i = 6; i > 0; i--){
             System.out.println(i + "! " + f.compoteI(i));
         }
-
+        System.out.println("Факториал рассчитаный при помощий рекурсии:");
+        for (int j = 1; j <= 6; j++){
+            System.out.println(j + "! " + f.computeR(j));
+        }
     }
 }
