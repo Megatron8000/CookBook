359a3f6b3bb6cac56d4494ef01d36b256007c779
Megatron8000 <barykin88@hotmail.com>
2022-05-31 09:18:22 +0400

    242424

---
+++ src/module14/BubbleSortDemo.java
@@ -0,0 +1,13 @@
+package module14;
+
+public class BubbleSortDemo {
+    private static int i;
+
+    public static void foo() {
+        int nums[] = {-99, 10, 64, 65432, -684, 88, -66, 47, -32};
+
+        System.out.print("Исходный порядок массива:");
+        for (int i = 0; i < nums.length; i++);
+        System.out.print(nums[i]);
+    }
+}
