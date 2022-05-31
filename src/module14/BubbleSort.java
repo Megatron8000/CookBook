a3e2d517c1d743107785a1bc4a755758c5be1d1f
Megatron8000 <barykin88@hotmail.com>
2022-05-31 09:13:09 +0400

    242424

--- src/module14/BubbleSort.java
+++ src/module14/BubbleSort.java
@@ -1,5 +1,19 @@
 package module14;
 
 public class BubbleSort {
-
+        static void sort(int nums[]){
+            int t; //вспомогательная переменная
+            for (int a = 1; a < nums.length; a++){
+                //повтор каждого элемента массива
+                for (int b = nums.length - 1; b >= a; b--){
+                    //повтор для проверки порядка соседних элементов
+                    if (nums[b - 1] > nums[b]){
+                        //перестановка элементов
+                        t = nums[b-1];
+                        nums[b -1] = nums[b];
+                        nums[b] = t;
+                    }
+                }
+            }
+        }
 }
