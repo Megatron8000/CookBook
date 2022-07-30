87e85788a9535972685ead85081323c7b9f2cfc7
Megatron8000 <barykin88@hotmail.com>
2022-07-30 08:54:08 +0400

    987641645

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -234,18 +234,7 @@ public class Main {
       QuickSortDemo qsd = new QuickSortDemo();
       qsd.foo();*/
 
-   Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
-    int c = scanner.nextInt();
 
-    if (a > b & a > c){
-        System.out.println(a);
-    } else if(b > a & b > c){
-        System.out.println(b);
-    } else {
-        System.out.println(c);
-    }
     } //method main
   }//class Main
 
