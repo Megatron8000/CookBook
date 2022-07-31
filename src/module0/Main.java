9068d2d38824de38c7307d1afdaeb054f45d9f9f
Megatron8000 <barykin88@hotmail.com>
2022-07-31 10:42:22 +0400

    21254545645454

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -233,12 +233,11 @@ public class Main {
 
       QuickSortDemo qsd = new QuickSortDemo();
       qsd.foo();*/
-    boolean a = true;
-    boolean b = false;
-    int c = 30;
-    int q = 5;
-    boolean r = (a || b && c >= 25) || (c < 20 * q) && !(q == 5);
-    System.out.println(r);
+
+    String s = "glass table";
+    String val = "val";
+    boolean b = !s.contains(val) || !val.equals("tab");
+    System.out.println(b);
 
   }//method main
     }   //class Main
