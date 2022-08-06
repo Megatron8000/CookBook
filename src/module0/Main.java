a646d75b694848192893d601e37444928e6371c2
Megatron8000 <barykin88@hotmail.com>
2022-08-06 11:34:58 +0400

    231313131

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -235,7 +235,13 @@ public class Main {
       qsd.foo();*/
 
    Scanner scanner = new Scanner(System.in);
-
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    if((a+b)%2 == 0 ){
+        System.out.println(true);
+    } else {
+        System.out.println(false);
+    }
 
 
   }//method main
