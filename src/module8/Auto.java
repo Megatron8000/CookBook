bb4a93b45f8f164ec090b0485b1f1605f2004fd9
Megatron8000 <barykin88@hotmail.com>
2023-01-03 12:46:57 +0400

    12121212112

--- src/module8/Auto.java
+++ src/module8/Auto.java
@@ -6,9 +6,9 @@ import module4.Vehicle;
 public class Auto extends Vehicle implements Movable {
     private boolean sunroof;
 
-       public Auto(boolean sunroof){
+      /* public Auto(boolean sunroof){
             super(330, "green", 4); // вызов конструктора класса Vehicle
-        }
+        }*/
 
       public Auto (int maxspeed, String color, int wheels, boolean sunroof){  //конструктор Auto с параметрами
            super(maxspeed, color, wheels);
