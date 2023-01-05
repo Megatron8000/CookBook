fee69780049b3f0914d0f41a07e8ad5c15165830
Megatron8000 <barykin88@hotmail.com>
2023-01-05 11:42:33 +0400

    32323232

--- src/module0/Main.java
+++ src/module0/Main.java
@@ -22,7 +22,7 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
 
-    System.out.println("It's alive! It's alive!");
+    System.out.println("Hello World!");
 
 
 
